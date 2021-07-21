4b4ecb822b73bb3ebc93d1ee669374988024686b
root <root@server2.odd.ucr.ac.cr>
2021-07-20 22:41:50 -0600

    Ajuste en archivos de vacunación

--- crawler/vacunas/vacunas/spiders/vacunasspider.py
+++ crawler/vacunas/vacunas/spiders/vacunasspider.py
@@ -9,6 +9,6 @@ class VacunasSpider(scrapy.Spider):
         item_total_vacunas = response.css('.marco h2.counter::text').getall()[0]
         item_primera_dosis = response.css('.marco h2.counter::text').getall()[1]
         item_segunda_dosis = response.css('.marco h2.counter::text').getall()[2]
-        item_fecha = response.css('.row.fuente p::text').getall()[1].split(' ')[1]
+        item_fecha = response.css('.row.fuente span::text').getall()[1].split(' ')[1]
         vacunasItem = VacunasItem(total_vacunas = item_total_vacunas, primera_dosis = item_primera_dosis, segunda_dosis = item_segunda_dosis, fecha = item_fecha)
         yield vacunasItem
